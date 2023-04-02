a7c6305b39f701e063391ad3491d562d26de0da2
Denisov Nurislam <96141958+NurikDen@users.noreply.github.com>
2023-04-02 17:29:04 +0300

    Month changes

---
+++ Assets/Scripts/ThunderDown.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderDown : MonoBehaviour
+   {
+[SerializeField]
+private float fallspeed = 3f;
+public GameObject gameObject;
+    void Update() {
+                  
+      transform.position -= new Vector3(0,fallspeed * Time.deltaTime ,0);
+           
+    }
+            
+ 
+ 
+}
+
